c46a1e8068ae9feefd9d69ff68cb253ee57ab879
davigbso@hotmail.com <davigbso@hotmail.com>
2025-04-27 16:06:50 -0300

    Remove duplicate chars in a string

---
+++ RemoveDuplicateCharsString.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * The string may have two or more same characters in it but we want it to have only one.
+ */
+
+Console.WriteLine("Input");
+string? input = Console.ReadLine();
+
+Console.WriteLine(RemoveDuplicated(input));
+
+static string RemoveDuplicated(string input)
+{
+  var eachChar = "";
+
+  var finalResult = "";
+
+  foreach (char value in input)
+  {
+
+    if (eachChar.Contains(value) == false)
+    {
+      eachChar += value;
+      finalResult += value;
+    }
+  }
+  return finalResult;
+}
+
