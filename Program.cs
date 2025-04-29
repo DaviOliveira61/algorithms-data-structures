ddebac0cef4011aa2e13536c097e0a6d70d798c8
davigbso@hotmail.com <davigbso@hotmail.com>
2025-04-29 12:18:01 -0300

    Change gitignore, implement binary search

--- Program.cs
+++ Program.cs
@@ -16,4 +16,21 @@ Console.Clear();
 // Fibonacci sequence starting from 0 and using a recursive method
 //Fibonacci.CallRecursively(30);
 
+// Binary search
+/* Binary Search is a widely used searching algorithm that efficiently finds the position of a target value 
+   within a sorted array. 
+   It works by repeatedly dividing the search interval in half. */
+
+/*int[] sortedArray = { 1, 2, 3, 6, 9, 10, 23, 105 };
+int result = BinarySearch.Search(sortedArray, 203);
+
+if (result != -1)
+    Console.WriteLine("Target found at index: " + result);
+else
+    Console.WriteLine("Target not found!"); */
+
+
+
+
+
 Console.ReadKey();
